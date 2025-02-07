b752c8fe885e1aef0b8056112df091029520df00
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-02-07 17:14:44 +0530

    Add rate limiting & bot detection using arcjet

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import express from "express";
 
+import { aj } from "./lib/arcjet.js";
 import { initDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
 
@@ -17,6 +18,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet()); // helmet is used to set security headers
 app.use(morgan("dev")); // morgan is used to log requests
+
+// arcjet rate limiting
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specify that each request consumes 1 token
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot detected, access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden, access denied" });
+      }
+      return;
+    }
+
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log(`Arcjet error: ${error}`);
+    next(error);
+  }
+});
 
 // routes
 app.use("/api/products", productRoutes);
